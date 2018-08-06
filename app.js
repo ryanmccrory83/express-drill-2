cc038ecdbdd66d02e9fe28d8151551f62f9be3e7
Ryan McCrory <ryanmccrory83@gmail.com>
2018-08-06 15:41:41 -0600

    completed express drill 2

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = parseInt(process.env.PORT || 3000);
+const csvToJson = require('convert-csv-to-json');
+const data = csvToJson.fieldDelimiter(',').getJsonFromCsv('instructors.csv');
+
+app.use(cors());
+
+function findById(data, id){
+    for (let i = 0; i < data.length; i++){
+        if (data[i].id === id){
+            return data[i];
+        }
+    }return null;
+}
+
+app.get('/', (request,response) => {
+    response.json({data: data});
+});
+
+app.get('/:id', (request,response) => {
+    var instructor = findById(data, request.params.id);
+    if (!instructor){
+        response.status(404).send({
+            error: {
+                message: "No record found!"
+            }
+        })
+    }else {
+        response.json({data: instructor});
+    }
+});
+
+app.listen(port);
